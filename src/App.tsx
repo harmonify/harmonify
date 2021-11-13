7278ae83d3f584a47175d939d4edf9942c174869
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-11-14 00:48:18 +0700

    restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { About } from './components/About';
-import { Experience } from './components/Experience';
 import { Footer } from './components/Footer';
 import { GithubCorners } from './components/GithubCorners';
 import { Header } from './components/Header';
-import { Projects } from './components/Projects';
-import { Skills } from './components/Skills';
+import { Projects } from './components/Projects/Projects';
+import { Skills } from './components/Skills/Skills';
 
 function App() {
   return (
     <div className="flex flex-col">
       <GithubCorners />
+      
       <Header />
       <About />
       <Projects />
       <Skills />
-      <Experience />
       <Footer />
     </div>
   );
