947c879d6cd95b3ad01661cebee2de5e394bc153
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-03-07 00:35:41 +0700

    update configs

--- src/config/APP.ts
+++ src/config/APP.ts
@@ -1,16 +1,15 @@
-import { AppInterface } from '../interfaces/AppInterface';
+import { AppInterface } from "../interfaces/AppInterface";
 
 export const APP: AppInterface = {
   name: "Wendy Surya Wijaya",
-  description: "Wendy Surya Wijaya is a freelance web developer from Indonesia.",
+  description:
+    "Wendy Surya Wijaya is a freelance web developer from Indonesia.",
   version: "1.0.0",
   src: "https://www.github.com/harmonify/harmonify",
   homepage: "https://harmonify.netlify.app",
   author: {
     name: "Wendy Surya Wijaya",
-    roles: [
-      "freelance web developer",
-    ],
+    roles: ["freelance web developer"],
     nationality: "Indonesia",
     about: [
       "I'm Wendy, a web developer based in Pontianak, Indonesia. I'm currently majoring in Informatics Engineering at STMIK Pontianak.",
@@ -18,84 +17,101 @@ export const APP: AppInterface = {
     ],
     avatar: "/img/avatar.jpg",
     email: "wendy18.sw@gmail.com",
-    projects: [{
+    projects: [
+      {
         title: "Web Portfolio",
         description: "A web portfolio created using React and TypeScript",
-        image: "https://raw.githubusercontent.com/harmonify/harmonify/main/public/img/banner.png",
+        image:
+          "https://raw.githubusercontent.com/harmonify/harmonify/main/public/img/banner.png",
         src: "https://www.github.com/harmonify/harmonify",
         demo: "https://harmonify.netlify.app",
       },
       {
         title: "Harmonify Blog",
         description: "A personal blog created using Laravel 8",
-        image: "https://raw.githubusercontent.com/harmonify/harmonify-blog/main/.github/img/preview.png",
+        image:
+          "https://raw.githubusercontent.com/harmonify/harmonify-blog/main/.github/img/preview.png",
         src: "https://www.github.com/harmonify/harmonify-blog",
         demo: "https://harmonify-blog.herokuapp.com/",
       },
       {
         title: "Random Quote Machine",
-        description: "A random quote machine created using React, styled with Bootstrap",
-        image: "https://raw.githubusercontent.com/harmonify/random-quote-machine/main/.github/img/preview.png",
+        description:
+          "A random quote machine created using React, styled with Bootstrap",
+        image:
+          "https://raw.githubusercontent.com/harmonify/random-quote-machine/main/.github/img/preview.png",
         src: "https://www.github.com/harmonify/random-quote-machine/",
         demo: "https://harmonify-rqm.netlify.app/",
       },
       {
         title: "Markdown Previewer",
         description: "Markdown Previewer styled with TailwindCSS",
-        image: "https://raw.githubusercontent.com/harmonify/markdown-previewer/main/.github/img/screenshot.png",
+        image:
+          "https://raw.githubusercontent.com/harmonify/markdown-previewer/main/.github/img/screenshot.png",
         src: "https://www.github.com/harmonify/markdown-previewer/",
         demo: "https://harmonify-mp.netlify.app/",
       },
       {
         title: "Drum Machine",
         description: "A drum machine created using React",
-        image: "https://raw.githubusercontent.com/harmonify/drum-machine/main/.github/img/preview.png",
+        image:
+          "https://raw.githubusercontent.com/harmonify/drum-machine/main/.github/img/preview.png",
         src: "https://www.github.com/harmonify/drum-machine/",
         demo: "https://harmonify-dm.netlify.app/",
       },
       {
         title: "Pomodoro Clock",
         description: "25 + 5 clock with React and TypeScript",
-        image: "https://raw.githubusercontent.com/harmonify/pomodoro-clock/main/.github/img/preview.png",
+        image:
+          "https://raw.githubusercontent.com/harmonify/pomodoro-clock/main/.github/img/preview.png",
         src: "https://www.github.com/harmonify/pomodoro-clock/",
         demo: "https://harmonify-pc.netlify.app/",
       },
       {
         title: "JavaScript Calculator",
         description: "Basic calculator with React and TypeScript",
-        image: "https://raw.githubusercontent.com/harmonify/javascript-calculator/main/.github/img/preview.png",
+        image:
+          "https://raw.githubusercontent.com/harmonify/javascript-calculator/main/.github/img/preview.png",
         src: "https://www.github.com/harmonify/javascript-calculator/",
         demo: "https://harmonify-jc.netlify.app/",
       },
       {
         title: "Keyboard Shop",
         description: "A keyboard shop with native PHP",
-        image: "https://raw.githubusercontent.com/harmonify/keyboard-shop/main/.github/img/index-page.png",
+        image:
+          "https://raw.githubusercontent.com/harmonify/keyboard-shop/main/.github/img/index-page.png",
         src: "https://www.github.com/harmonify/keyboard-shop/",
-      }
+      },
     ],
-    workExperiences: [{
-      title: "Freelance Web Developer",
-      subtitle: "projects.co.id",
-      date: "May 2021 - present",
-      description: "I've been working as a freelance web developer for more than half a year. I've been working on projects such as:",
-      items: [{
-          name: "Building static and dynamic web pages for client",
-        },
-        {
-          name: "Handling CRUD operations for client's CRM"
-        },
-      ]
-    }],
-    studyExperiences: [{
+    workExperiences: [
+      {
+        title: "Freelance Web Developer",
+        subtitle: "projects.co.id",
+        date: "May 2021 - present",
+        description:
+          "I've been working as a freelance web developer for more than half a year. I've been working on projects such as:",
+        items: [
+          {
+            name: "Building static and dynamic web pages for client",
+          },
+          {
+            name: "Handling CRUD operations for client's CRM",
+          },
+        ],
+      },
+    ],
+    studyExperiences: [
+      {
         title: "freeCodeCamp Certifications",
         titleLink: "https://www.freecodecamp.org/",
         subtitle: "freeCodeCamp",
         date: "May 2021 - present",
-        description: "Learn how to make a responsive web design with great accessibility by using some of the most popular front end libraries in the industry.",
-        items: [{
+        description:
+          "Learn how to make a responsive web design with great accessibility by using some of the most popular libraries and frameworks in the industry.",
+        items: [
+          {
             name: "Responsive Web Design",
-            link: "https://www.freecodecamp.org/certification/harmonify/responsive-web-design"
+            link: "https://www.freecodecamp.org/certification/harmonify/responsive-web-design",
           },
           {
             name: "JavaScript Algorithms and Data Structures",
@@ -105,15 +121,33 @@ export const APP: AppInterface = {
             name: "Front End Development Libraries",
             link: "https://www.freecodecamp.org/certification/harmonify/front-end-development-libraries",
           },
+          {
+            name: "Data Visualization",
+            link: "https://www.freecodecamp.org/certification/harmonify/data-visualization",
+          },
+          {
+            name: "Scientific Computing with Python",
+            link: "https://www.freecodecamp.org/certification/harmonify/scientific-computing-with-python-v7",
+          },
+          {
+            name: "Back End Development and APIs",
+            link: "https://www.freecodecamp.org/certification/harmonify/back-end-development-and-apis",
+          },
+          {
+            name: "Quality Assurance",
+            link: "https://www.freecodecamp.org/certification/harmonify/quality-assurance-v7",
+          },
         ],
       },
       {
         title: "IDCamp 2021",
         titleLink: "https://idcamp.indosatooredoo.com/",
         subtitle: "Indosat Ooredoo & Dicoding Indonesia",
-        description: "Introduction to the cloud computing, develop back-end APIs using Node.js, and learn best practices along the way.",
+        description:
+          "Introduction to the cloud computing, develop back-end APIs using Node.js, and learn best practices along the way.",
         date: "July 2021 - present",
-        items: [{
+        items: [
+          {
             name: "Cloud Practitioner Essentials",
             link: "https://www.dicoding.com/certificates/2VX32106JPYQ",
           },
@@ -125,63 +159,37 @@ export const APP: AppInterface = {
             name: "Belajar Prinsip Pemrograman SOLID",
             link: "https://www.dicoding.com/certificates/0LZ0358YQZ65",
           },
-        ]
+        ],
       },
     ],
     socials: {
       github: "https://www.github.com/harmonify",
       linkedin: "https://www.linkedin.com/in/wendy-surya-wijaya",
     },
-    skills: [{
+    skills: [
+      {
         title: "HTML and CSS",
-        items: [
-          "HTML",
-          "CSS",
-          "Bootstrap",
-          "Tailwind CSS",
-          "SASS",
-        ],
+        items: ["HTML", "CSS", "Bootstrap", "Tailwind CSS", "SASS"],
       },
       {
         title: "Programming Languages",
-        items: [
-          "JavaScript",
-          "TypeScript",
-          "PHP",
-          "Python",
-        ],
+        items: ["JavaScript", "TypeScript", "PHP", "Python"],
       },
       {
         title: "Database",
-        items: [
-          "MySQL",
-          "MongoDB",
-        ],
+        items: ["MySQL", "MongoDB"],
       },
       {
         title: "Frameworks",
-        items: [
-          "Laravel",
-          "Node.js",
-          "React",
-          "Angular",
-          "Express",
-        ],
+        items: ["Laravel", "Node.js", "React", "Angular", "Express"],
       },
       {
         title: "Tools",
-        items: [
-          "Git",
-          "GitHub",
-          "Postman",
-          "Docker",
-        ],
+        items: ["Git", "GitHub", "Postman", "Docker"],
       },
       {
         title: "Others",
-        items: [
-          "Linux (basic command-line operations)",
-        ],
+        items: ["Linux (basic command-line operations)"],
       },
     ],
   },
